a0e5c4b3bd28ff6b44723bed6335457666afcdbf
James Collerton <james.collerton@bbc.co.uk>
2019-07-17 09:34:16 +0100

    Added a small amount more templating

--- meadowlark/meadowlark.js
+++ meadowlark/meadowlark.js
@@ -13,13 +13,11 @@ app.set('view engine', 'handlebars');
 app.set('port', process.env.PORT || 3000);
 
 app.get('/', function(req, res){
-    res.type('text/plain');
-    res.send('Meadowlark Travel');
+    res.render('home');
 });
 
 app.get('/about', function(req, res){
-    res.type('text/plain');
-    res.send('About Meadowlark Travel');
+    res.render('about');
 });
 
 /*
@@ -29,17 +27,15 @@ app.get('/about', function(req, res){
 
 // custom 404 page
 app.use(function(req, res){
-    res.type('text/plain');
     res.status(404);
-    res.send('404 - Not Found');
+    res.render('404');
 });
 
 // custom 500 page
 app.use(function(err, req, res, next){
     console.error(err.stack);
-    res.type('text/plain');
     res.status(500);
-    res.send('500 - Server Error');
+    res.render('500');
 });
 
 app.listen(app.get('port'), function() {
